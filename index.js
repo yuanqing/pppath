485567414b21e8a845f7517924decf3510480ded
yuanqing <hello@yuanqing.sg>
2014-12-29 12:09:47 +0800

    Refactor; add comments

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 'use strict';
 
-var consecutiveSlash = /(:)?\/+/g;
+// cache the RegExps we need
 var endsWithExtension = /[^\/]*\.[^\/]+$/g;
+var consecutiveSlash = /(:)?\/+/g; // capture the ':'
 
+// replace match with '://' if we've captured `c` (ie. the ':'), else replace with '/'
 var fn = function(match, c) {
   return c ? '://' : '/';
 };
 
 var pppath = function(parts, filename) {
-  if (typeof parts === 'string') {
-    parts = [parts];
-  }
+
+  // ensure `parts` is an array
+  parts = [].concat(parts);
+
+  // append `filename` only if last element of `parts` does not end with extension
   if (filename && parts[parts.length-1].search(endsWithExtension) === -1) {
     parts.push(filename);
   }
-  return parts.filter(Boolean).join('/').replace(consecutiveSlash, fn);
+
+  return parts.filter(Boolean) // filter out falsy
+              .join('/')
+              .replace(consecutiveSlash, fn); // remove consecutive '/'
+
 };
 
-module.exports = exports = pppath;
+module.exports = pppath;
